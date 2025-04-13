61feb463c9f17d30294bb519fc1258a8598f8ac5
fabrizio <larosaf87@gmail.com>
2025-04-13 18:35:10 +0200

    updated main

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include "includes/mem.h"
+#include <stdio.h>
 
 int main() {
-  int *num1 = (int *)(calloc(5, sizeof(int)));
+  int *num1 = (int *)(malloc(sizeof(int) * 5)); //allocates size for 5 integers
+  free(num1); //frees memory
 
-  for (int i = 0; i < 5; i++){
-    printf("%d", num1[i]);
-  }
+  int *num2 = (int *)(calloc(5, sizeof(int))); //allocates size for an array of "5" elements of "int" size
+  int *num3 = (int *)(realloc(num2, sizeof(int) * 10)); //reallocates memory from holding "5" integers to holding "10"
+
+  free(num2); //frees memory
+  free(num3); //frees memory
 }
